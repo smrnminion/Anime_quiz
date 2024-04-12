b61e18e0de3d2257d3e3a9ea46f2cd0e93f919a0
smrnminion <smrn2002@yandex.ru>
2024-04-12 23:40:22 +0300

    debug

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -153,7 +153,7 @@
     padding: calc(7px + (15 - 7) * ((150vw - 200px) / (1440 - 200)));
     background: var(--plasma-colors-buttonAccent);
     color: var(--plasma-colors-buttonPrimary);
-    font-weight: 200;
+    font-weight: 600;
     font-size: calc(8px + (18 - 12) * ((150vw - 200px) / (1440 - 200)));
     transition: background ease 0.5s;
     border: 1px solid transparent;
