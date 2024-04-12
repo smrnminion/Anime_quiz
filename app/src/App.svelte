d46e49dccca8f31888826c2f45078489ec899aec
smrnminion <smrn2002@yandex.ru>
2024-04-12 23:37:00 +0300

    debug

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -140,7 +140,7 @@
     border: 1px solid var(--plasma-colors-buttonWarning)
   }
   .buttons {
-    display: flex;
+    display: grid;
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
